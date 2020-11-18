7558d4277f4b44047b8b1dcf0babecb94e77958e
nourhansaied <nourhan>
2020-11-18 20:45:05 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* window.alert()
+ i love alexandria
+*/
+// document.getElementById('test').innerHTML = 'I love' + document.getElementById('test').innerHTML;
+// console.log(document.getElementById('test'))
+
+// variables lossly typed
+// strong typed 
+
+// var userName = 'Ahmed'
+
+
+// var 7up = 'Ahmed'
+// var user name = 'Ahmed'
+// var $user = 'Ahmed'
+// var username = 'Ahmed'
+// var function = 'Ahmed'
+
+
+
+//Data type primitive 
+// string -> ' ' , "" 
+// number
+// boolean
+// undefined
+// null
+
+
+var userName = "Ahmed";
+var nameContent = document.getElementById('name');
+nameContent.innerHTML = 
+nameContent.innerHTML + userName
+console.log(document.getElementById('name').innerHTML)
+var age = 27;
+
+document.getElementById('age').innerHTML =
+    document.getElementById('age').innerHTML + age
+
+
+var isStudent = false;
+userName = isStudent;
+var x= null;
+document.getElementById('student').innerHTML =
+    document.getElementById('student').innerHTML + isStudent
+
+document.getElementById('test').innerHTML = typeof x;
+
+
+x = 10;
